0a1cba92dbc319a989b6ad241bb0f5e4f07b9df4
Your Name <you@example.com>
2022-01-20 20:23:08 +0200

    add project

---
+++ bmi_app/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'bmi_screen.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+// Stateless
+// Stateful
+
+// class MyApp
+
+class MyApp extends StatelessWidget
+{
+  // constructor
+  // build
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BmiScreen(),
+    );
+  }
+}
